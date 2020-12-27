a4be94fc6449c2a9711e7d845b200afe6c3dd88b
Keisuke Matsuo <ksk.matsuo12@gmail.com>
2020-12-27 14:09:07 +0900

    レイアウト途中まで

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,12 +61,39 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+
+            .container {
+                display: flex;
+                width: 95%;
+                max-width: 1140px;
+                margin: 0 auto;
+            }
+
+            .top_image {
+                margin: 0 auto;
+                width: 200px;
+                height: 300px;
+            }
+
+            .top_image img {
+                width: 200px;
+                height: 300px;
+                object-fit: cover;
+            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                
+            @endif
+            <div class="container">
+                <div class="top_image">
+                    <img src="{{ asset('top_img.jpg') }}"/>
+                </div>
+            <div>
+                <div>
+                <div class="">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -77,24 +104,10 @@
                         @endif
                     @endauth
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
+        </div>
+     
     </body>
 </html>
