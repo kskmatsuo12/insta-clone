fe4c85f23ca4c0d7c9fa6610befb521b8625ebdb
Keisuke Matsuo <ksk.matsuo12@gmail.com>
2020-12-27 14:18:03 +0900

    一旦保存

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,48 +20,6 @@
                 margin: 0;
             }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
             .container {
                 display: flex;
                 width: 95%;
@@ -83,9 +41,8 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                
+        <div class="">
+            @if (Route::has('login'))       
             @endif
             <div class="container">
                 <div class="top_image">
@@ -108,6 +65,5 @@
             </div>
         </div>
         </div>
-     
     </body>
 </html>
